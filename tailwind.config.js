7d331f9b1cb2fb8c2dd0eb4a989061cc791c0c24
Yestoya Minggus <yestoya.minggus@gmail.com>
2024-09-11 20:36:30 +0700

    base code

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+
+    fontFamily:{
+
+    },
+
+    extend: {
+      colors: {
+        'lemon': '#FFF0C2',
+        'charcoal': '#25424C',
+        'coral': '#FF8552',
+        'sienna': '#942911',
+        'falu': '#772014'
+
+      }
+    },
+  },
+  plugins: [],
+}
